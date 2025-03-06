ec0af9714b39bbb4a82b2ebfda32b17f3350b6fd
MedHK008 <mohammedherak37@gmail.com>
2025-03-06 21:11:50 +0000

    remove aladhan module and related configurations

--- aladhan/routes/buildingsRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getBuildings } from "../controllers/buildingsControllers";
-
-const buildingRoute = express.Router();
-
-buildingRoute.get('/', getBuildings);
-export default buildingRoute;
