c7e97c777a74189e37c14efa132312d7826eb1dd
Salah1779 <salahbusiness18@gmail.com>
2025-03-24 07:40:22 +0000

    refactor zone model and controller to enhance zone processing and include additional properties

--- zone/controllers/zoneController.ts
+++ zone/controllers/zoneController.ts
@@ -1,30 +1,47 @@
 import { zoneModule } from "../modules/zoneModule"
 import { Request, Response } from "express";
 interface ProcessedZone {
-    id: string;
+    zoneId: string;
     type: string;
-
-    geometry?: Array<{ lat: number; lon: number }>; // Add geometry to response
+    geometry: { lat: number; lon: number }[];
+    bounding_box?: {
+        minLat:number,
+        maxLat:number,
+        minLon:number,
+        maxLon:number};
+    buildings?:string[],
+    routes?:string[],
+    cross_walks?:number 
+    car?: number
+    pedestrian?: number
 }
 const zoneToProcessedZone = (zones: any[]): ProcessedZone[] => {
     const processedZones: ProcessedZone[] = zones.map((zone): ProcessedZone => {
         return {
-            id: zone.id,
+            zoneId: zone._id || "none",
             type: zone.tags.landuse || zone.tags.leisure || zone.tags.natural || 'unknown',
-            geometry: zone.geometry // Include geometry in the response
+            geometry: zone.geometry,
+            //bounding_box: {minLat : zone.bounds.minLat , maxLat : zone.bounds.maxLat , minLon : zone.bounds.minLon , maxLon : zone.bounds.maxLon},
+            cross_walks: zone.cross_walks || undefined,
+            routes: zone.routes,
+            buildings: zone.buildings
+    
         };
+
     });
+
     return processedZones;
 }
 export const getZones = async (req: Request, res: Response) => {
     try {
         const data = await zoneModule.find({});
         const processedZones = zoneToProcessedZone(data);
+        console.log(processedZones[0]);
         res.status(200).json({ success: true, processedZones })
-    } catch (error) {
-        res.status(500).json({ success: false });
-    }
 
+    } catch (error) {
+        res.status(500).json({ success: false , error: error });
+    }
 }
 
 
--- zone/modules/zoneModule.ts
+++ zone/modules/zoneModule.ts
@@ -1,33 +1,23 @@
 import mongoose from "mongoose";
-const zoneSchema = new mongoose.Schema(
+const zoneSchema = new mongoose.Schema
+    ({
+        zoneId: { type: String, required: true },
+        geometry: [{ lat: { type: Number, required: true }, lon: { type: Number, required: true } }],
+        bounding_box: [
 
-    {
-        id: { type: Number, required: true },
-        type: { type: String, required: true },
-        bounds: {
-            minlat: { type: Number, required: true },
-            minlon: { type: Number, required: true },
-            maxlat: { type: Number, required: true },
-            maxlon: { type: Number, required: true },
-        },
-        geometry: {
-            type: [
-                {
-                    lat: { type: Number, required: true },
-                    lon: { type: Number, required: true },
-                }
-            ],
-            required: true
-        },
+            {
+                lat: { type: Number, required: true },
+                lon: { type: Number, required: true },
+            }
+        ],
+
+
         tags: {
-            natural: { type: String },
-            landuse: { type: String },
-            leisure: { type: String },
-        },
-        buildings: [{ type: Number, required: true }],
-        routes: [{ type: Number, required: true }],
-        trafficLights: [{ type: Number, required: true }],
+            landuse: { type: String, required: false },
 
-    }
-)
+        },
+        buildings: [{ type: mongoose.Schema.Types.ObjectId, ref: "buildings", required: true }],
+        routes: [{ type: mongoose.Schema.Types.ObjectId, ref: "routes", required: true }],
+        cross_walks: { type: Number, required: true },
+    });
 export const zoneModule = mongoose.models.zones || mongoose.model("zone", zoneSchema);
